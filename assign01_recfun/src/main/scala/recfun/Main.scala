c9613c8527ef62224f67253c48b014f8841dbfe8
neil <0xbdf1@gmail.com>
2014-09-28 19:43:24 -0700

    implementations of assign01

--- assign01_recfun/src/main/scala/recfun/Main.scala
+++ assign01_recfun/src/main/scala/recfun/Main.scala
@@ -14,15 +14,41 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if(c < 0 || c > r) 0
+    else if(r == 0) 1
+    else pascal(c, r-1) + pascal(c-1, r-1)
+  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def helper(chars: List[Char], openCount: Int): Int = {
+      if(chars.isEmpty) openCount
+      else if(chars.head == '(') helper(chars.tail, openCount+1)
+      else if(chars.head == ')') {
+        if(openCount > 0) helper(chars.tail, openCount-1)
+        else -1
+      }
+      else helper(chars.tail, openCount)
 
+    }
+    def openCount:Int = helper(chars, 0)
+    openCount == 0
+  }
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+
+  def countChange(money: Int, coins: List[Int]): Int = {
+    def sortedCoins = coins.sortWith(_ > _)
+    def helper(money: Int, coins: List[Int]): Int = {
+      if(coins.isEmpty) 0
+      else if(money - coins.head == 0) 1 + helper(money, coins.tail)
+      else if (coins.head > money) helper(money, coins.tail)
+      else helper(money-coins.head, coins) + helper(money, coins.tail)
+    }
+    helper(money, sortedCoins)
+  }
 }
